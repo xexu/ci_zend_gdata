29f61dfb3616191bc65c46b3c76610a076f7d650
latuminggi <git@latuminggi.com>
2014-01-29 11:21:34 +0700

    Update zend_helper.php

--- application/helpers/zend_helper.php
+++ application/helpers/zend_helper.php
@@ -2,4 +2,4 @@
 // $path_to_zend = str_replace('/', '\\', APPPATH) . 'zend\\'; //windows
 // $path_to_zend = APPPATH . '/zend/'; //linux
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . $path_to_zend);
-require_once ('Zend/Loader.php');
+require_once ($path_to_zend .'Zend/Loader.php');
